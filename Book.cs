171ef8823a9e1bbb3cf0e68ffe8ca198b34e5ad2
khadijafassih <144409536+khadijafassih@users.noreply.github.com>
2024-02-07 00:37:50 +0500

    week3
    task4

---
+++ Book.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDTask4.BL
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public int PublicationYear;
+        public float Price;
+        public int QuantityInStock;
+
+        public Book(string title, string author, int publicationYear, float price, int quantityInStock)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+            Price = price;
+            QuantityInStock = quantityInStock;
+        }
+
+        public string GetTitle()
+        {
+            return $"Title: {Title}";
+        }
+
+        public string GetAuthor()
+        {
+            return $"Author: {Author}";
+        }
+
+        public string GetPublicationYear()
+        {
+            return $"Publication Year: {PublicationYear}";
+        }
+
+        public string GetPrice()
+        {
+            return $"Price: {Price}";
+        }
+
+        public void SellCopies(int numberOfCopies)
+        {
+            if (numberOfCopies > QuantityInStock)
+            {
+                Console.WriteLine($"Error: Not enough copies in stock. Only {QuantityInStock} copies available.");
+            }
+            else
+            {
+                QuantityInStock -= numberOfCopies;
+            }
+        }
+
+        public void Restock(int additionalCopies)
+        {
+            QuantityInStock += additionalCopies;
+        }
+
+        public string BookDetails()
+        {
+            return $"Title: {Title}, Author: {Author}, Publication Year: {PublicationYear}, Price: {Price}, Quantity in Stock: {QuantityInStock}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,30 +3,135 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PDTask3.BL;
+using PDTask4.BL;
 
-namespace PDTask3
+namespace PDTask4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Shiritori my_shiritori = new Shiritori();
+            List<Book> bookList = new List<Book>
+            {
+                new Book("Pride and Prejudice", "Jane Austen", 1813, 19.99F, 100),
+                new Book("Hamlet", "William Shakespeare", 1603, 15.50F, 50),
+                new Book("War and Peace", "Leo Tolstoy", 1869, 25.99F, 75)
+            };
 
-            my_shiritori.Play("apple");
-            my_shiritori.Play("ear");
-            my_shiritori.Play("rhino");
-            my_shiritori.Play("corn");
-            Console.WriteLine("[" + string.Join(", ", my_shiritori.words) + "]");
-            Console.WriteLine(my_shiritori.Restart());
-            my_shiritori.Play("hostess");
-            my_shiritori.Play("stash");
-            Console.WriteLine("[" + string.Join(", ", my_shiritori.words) + "]");
-            my_shiritori.Play("hostess");
-            Console.WriteLine("[" + string.Join(", ", my_shiritori.words) + "]");
+            while (true)
+            {
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. View All Books Information");
+                Console.WriteLine("3. Get Author Details of a Specific Book");
+                Console.WriteLine("4. Sell Copies of a Specific Book");
+                Console.WriteLine("5. Restock a Specific Book");
+                Console.WriteLine("6. See the count of the Books Present in your BookList");
+                Console.WriteLine("7. Exit");
 
-            Console.ReadLine();
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter Title: ");
+                        string title = Console.ReadLine();
+                        Console.Write("Enter Author: ");
+                        string author = Console.ReadLine();
+                        Console.Write("Enter Publication Year: ");
+                        int publicationYear = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Price: ");
+                        float price = float.Parse(Console.ReadLine());
+                        Console.Write("Enter Quantity in Stock: ");
+                        int quantityInStock = int.Parse(Console.ReadLine());
+                        bookList.Add(new Book(title, author, publicationYear, price, quantityInStock));
+                        break;
+                    case 2:
+                        foreach (Book book in bookList)
+                        {
+                            Console.WriteLine(book.BookDetails());
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter Title: ");
+                        string searchTitle = Console.ReadLine();
+                        bool bookFound = false;
+                        foreach (Book book in bookList)
+                        {
+                            if (book.Title == searchTitle)
+                            {
+                                Console.WriteLine(book.GetAuthor());
+                                bookFound = true;
+                                break;
+                            }
+                        }
+
+                        if (!bookFound)
+                        {
+                            Console.WriteLine("Book not found.");
+                        }
+
+                        break;
+                    case 4:
+                        Console.Write("Enter Title: ");
+                        string sellTitle = Console.ReadLine();
+                        Book sellBook = null;
+
+                        foreach (Book book in bookList)
+                        {
+                            if (book.Title == sellTitle)
+                            {
+                                sellBook = book;
+                                break;
+                            }
+                        }
+                        if (sellBook != null)
+                        {
+                            Console.Write("Enter number of copies to sell: ");
+                            int sellCopies = int.Parse(Console.ReadLine());
+                            sellBook.SellCopies(sellCopies);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Book not found.");
+                        }
+                        break;
+                    case 5:
+                        Console.Write("Enter Title: ");
+                        string restockTitle = Console.ReadLine();
+                        Book restockBook = null;
+
+                        foreach (Book book in bookList)
+                        {
+                            if (book.Title == restockTitle)
+                            {
+                                restockBook = book;
+                                break;
+                            }
+                        }
+
+                        if (restockBook != null)
+                        {
+                            Console.Write("Enter number of additional copies to restock: ");
+                            int additionalCopies = int.Parse(Console.ReadLine());
+                            restockBook.Restock(additionalCopies);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Book not found.");
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine($"Number of books in the list: {bookList.Count}");
+                        break;
+                    case 7:
+                        Console.WriteLine("Exiting program...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
-        
+
