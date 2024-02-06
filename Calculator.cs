a3b9a8384fc2cf39527523f1aa871b43b159799e
khadijafassih <144409536+khadijafassih@users.noreply.github.com>
2024-02-07 00:26:25 +0500

    Add files via upload
    week3

---
+++ Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDtask1
+{
+    public class Calculator
+    {
+        public float Value1;
+        public float Value2;
+
+        public Calculator(float value1, float value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+        public Calculator()
+        {
+            this.Value1 = 0;
+            this.Value1 = 0;
+        }
+        public float Add()
+        {
+            float sum = Value1 + Value2;
+            return sum;
+        }
+        public float Subtract()
+        {
+            float subtract = Value1 - Value2;
+            return subtract;
+        }
+        public float Multiply()
+        {
+            float Product = Value1 * Value2;
+            return Product;
+        }
+        public float Divide()
+        {
+            if (Value2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                return 0;
+            }
+            return Value1 / Value2;
+        }
+        public float Modulo()
+        {
+            float Modulo = Value1 % Value2;
+            return Modulo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Challenge1;
 
-namespace problem1LAB1
+namespace PDtask1
 {
     internal class Program
     {
         static int Menu()
         {
-            Console.WriteLine("1. Add Student.");
-            Console.WriteLine("2. Show Students.");
-            Console.WriteLine("3. Calculate Aggregate.");
-            Console.WriteLine("4. Top Students.");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("1. Create the a Single Object of Calculator");
+            Console.WriteLine("2. Change Values of Attributes");
+            Console.WriteLine("3. Addition.");
+            Console.WriteLine("4. Subtraction.");
+            Console.WriteLine("5. Multiplication.");
+            Console.WriteLine("6. Division.");
+            Console.WriteLine("7. Modulo.");
             Console.Write("Enter your choice: ");
 
             if (int.TryParse(Console.ReadLine(), out int choice))
@@ -24,106 +25,51 @@ namespace problem1LAB1
             }
             return -1;
         }
-
-        static Student InputData()
+        public static void Main()
         {
-            Console.WriteLine("Enter Student Name");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Enter Student Matric Marks");
-            float matricMarks = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Student FSC Marks");
-            float fscMarks = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Student ECAT Marks");
-            float ecatMarks = float.Parse(Console.ReadLine());
-
-            return new Student(name, matricMarks, fscMarks, ecatMarks);
-        }
-
-        static void PrintData(Student[] studentsData)
-        {
-            Console.WriteLine("Name\t Matric Marks \t FSC Marks \t Ecat Marks \t Aggregate");
-            for (int i = 0; i < studentsData.Length; i++)
-            {
-                Console.WriteLine($"{studentsData[i].Name}\t {studentsData[i].MatricMarks}\t\t {studentsData[i].FscMarks}\t \t{studentsData[i].EcatMarks}\t\t {studentsData[i].Aggregate}");
-            }
-        }
-
-        static void AggregateCalculator(Student[] studentsData)
-        {
-            for (int i = 0; i < studentsData.Length; i++)
-            {
-                studentsData[i].Aggregate = (studentsData[i].FscMarks * 0.6F) + (studentsData[i].EcatMarks * 0.7F) + (studentsData[i].MatricMarks * 0.85F);
-            }
-        }
-
-        static void DisplayTopStudents(Student[] studentsData, int topCount)
-        {
-            Console.WriteLine($"Top {topCount} Students:");
-            for (int i = 0; i < studentsData.Length - 1; i++)
-            {
-                for (int j = 0; j < studentsData.Length - 1 - i; j++)
-                {
-                    if (studentsData[j].Aggregate < studentsData[j + 1].Aggregate)
-                    {
-                        Student temp = studentsData[j];
-                        studentsData[j] = studentsData[j + 1];
-                        studentsData[j + 1] = temp;
-                    }
-                }
-            }
-
-            // Display Top Students
-            for (int i = 0; i < topCount && i < studentsData.Length; i++)
-            {
-                Console.WriteLine($"{i + 1}. {studentsData[i].Name} - Aggregate: {studentsData[i].Aggregate}");
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            Student[] studentsData = new Student[3];
+            Calculator calculator = new Calculator();
             int choice;
-
-            while (true)
+            while ((choice = Menu()) != 8)
             {
-                choice = Menu();
-
                 switch (choice)
                 {
                     case 1:
-                        for (int i = 0; i < studentsData.Length; i++)
-                        {
-                            studentsData[i] = InputData();
-                        }
-                        AggregateCalculator(studentsData);
+                        Console.WriteLine("Enter two numbers: ");
+                        float value1 = float.Parse(Console.ReadLine());
+                        float value2 = float.Parse(Console.ReadLine());
+                        calculator = new Calculator(value1, value2);
                         break;
-
                     case 2:
-                        PrintData(studentsData);
+                        Console.WriteLine("Enter new values for Value1 and Value2: ");
+                        float newValue1 = float.Parse(Console.ReadLine());
+                        float newValue2 = float.Parse(Console.ReadLine());
+                        calculator.Value1 = newValue1;
+                        calculator.Value2 = newValue2;
                         break;
-
                     case 3:
-                        //AggregateCalculator(studentsData);
-                        PrintData(studentsData);
+                        Console.WriteLine("Addition: " + calculator.Add());
                         break;
-
                     case 4:
-                        DisplayTopStudents(studentsData, 3);
+                        Console.WriteLine("Subtraction: " + calculator.Subtract());
                         break;
-
                     case 5:
-                        Console.WriteLine("Exiting the program.");
-                        return;
-
-                    default:
-                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        Console.WriteLine("Multiplication: " + calculator.Multiply());
                         break;
+                    case 6:
+                        Console.WriteLine("Division: " + calculator.Divide());
+                        break;
+                    case 7:
+                        Console.WriteLine("Modulo: " + calculator.Modulo());
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                    Console.Read();
                 }
             }
         }
     }
 }
+    
+
 
