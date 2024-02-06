0ec90b9b7db967d69c4e355721fcad72f5dfe233
khadijafassih <144409536+khadijafassih@users.noreply.github.com>
2024-02-07 00:33:40 +0500

    week3
    task2

---
+++ Calculator2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDTask2.BL
+{
+    public class Calculator2
+    {
+
+        public float Value1;
+        public float Value2;
+
+        public Calculator2(float value1, float value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+        public Calculator2()
+        {
+            this.Value1 = 0;
+            this.Value2 = 0;
+        }
+        public Calculator2(float number)
+        {
+            Value1 = number;
+        }
+        public float Add()
+        {
+            float sum = Value1 + Value2;
+            return sum;
+        }
+        public float Subtract()
+        {
+            float subtract = Value1 - Value2;
+            return subtract;
+        }
+        public float Multiply()
+        {
+            float Product = Value1 * Value2;
+            return Product;
+        }
+        public float Divide()
+        {
+            if (Value2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                return 0;
+            }
+            return Value1 / Value2;
+        }
+        public float Modulo()
+        {
+            float Modulo = Value1 % Value2;
+            return Modulo;
+        }
+        public float Sqrt()
+        {
+            if (Value1 < 0)
+            {
+                Console.WriteLine("Error: Square root of negative number is not allowed.");
+                return 0;
+            }
+            return (float)Math.Sqrt(Value1);
+        }
+        public float Exp()
+        {
+            return (float)Math.Pow(Value2, Value1);
+        }
+        public float Log()
+        {
+            if (Value1 <= 0)
+            {
+                Console.WriteLine("Error: Logarithm of zero or negative number is not allowed.");
+                return 0;
+            }
+            return (float)Math.Log(Value1);
+        }
+        public float Sin()
+        {
+            float ans = (float)Math.Sin(Value2);
+            return ans * (180 / (float)(Math.PI));
+        }
+        public float Cos()
+        {
+            return (float)Math.Cos(Value2);
+        }
+        public float Tan()
+        {
+            return (float)Math.Tan(Value2);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PDTask2.BL;
 
-namespace PDtask1
+namespace PDTask2
 {
     internal class Program
     {
@@ -17,6 +18,13 @@ namespace PDtask1
             Console.WriteLine("5. Multiplication.");
             Console.WriteLine("6. Division.");
             Console.WriteLine("7. Modulo.");
+            Console.WriteLine("8. Square Root.");
+            Console.WriteLine("9. Exponential Function.");
+            Console.WriteLine("10.Logarithm.");
+            Console.WriteLine("11.Sine.");
+            Console.WriteLine("12.Cosine.");
+            Console.WriteLine("13.Tangent.");
+            Console.WriteLine("14.Exit.");
             Console.Write("Enter your choice: ");
 
             if (int.TryParse(Console.ReadLine(), out int choice))
@@ -25,11 +33,12 @@ namespace PDtask1
             }
             return -1;
         }
-        public static void Main()
+
+        static void Main(string[] args)
         {
-            Calculator calculator = new Calculator();
+            Calculator2 calculator2 = new Calculator2();
             int choice;
-            while ((choice = Menu()) != 8)
+            while ((choice = Menu()) != 14)
             {
                 switch (choice)
                 {
@@ -37,39 +46,57 @@ namespace PDtask1
                         Console.WriteLine("Enter two numbers: ");
                         float value1 = float.Parse(Console.ReadLine());
                         float value2 = float.Parse(Console.ReadLine());
-                        calculator = new Calculator(value1, value2);
+                        calculator2 = new Calculator2(value1, value2);
                         break;
                     case 2:
                         Console.WriteLine("Enter new values for Value1 and Value2: ");
                         float newValue1 = float.Parse(Console.ReadLine());
                         float newValue2 = float.Parse(Console.ReadLine());
-                        calculator.Value1 = newValue1;
-                        calculator.Value2 = newValue2;
+                        calculator2.Value1 = newValue1;
+                        calculator2.Value2 = newValue2;
                         break;
                     case 3:
-                        Console.WriteLine("Addition: " + calculator.Add());
+                        Console.WriteLine("Addition: " + calculator2.Add());
                         break;
                     case 4:
-                        Console.WriteLine("Subtraction: " + calculator.Subtract());
+                        Console.WriteLine("Subtraction: " + calculator2.Subtract());
                         break;
                     case 5:
-                        Console.WriteLine("Multiplication: " + calculator.Multiply());
+                        Console.WriteLine("Multiplication: " + calculator2 .Multiply());
                         break;
                     case 6:
-                        Console.WriteLine("Division: " + calculator.Divide());
+                        Console.WriteLine("Division: " + calculator2.Divide());
                         break;
                     case 7:
-                        Console.WriteLine("Modulo: " + calculator.Modulo());
+                        Console.WriteLine("Modulo: " + calculator2.Modulo());
+                        break;
+                    case 8:
+                        Console.WriteLine("SquareRoot: " + calculator2.Sqrt());
+                        break;
+                    case 9:
+                        Console.WriteLine("Exponential Function: " + calculator2.Exp());
+                        break;
+                    case 10:
+                        Console.WriteLine("Logarithm: " + calculator2.Log());
+                        break;
+                    case 11:
+                        Console.WriteLine("Sine: " + calculator2.Sin());
+                        break;
+                    case 12:
+                        Console.WriteLine("Cosine: " + calculator2.Cos());
+                        break;
+                    case 13:
+                        Console.WriteLine("Tangent: " + calculator2.Tan());
+                        break;
+                    case 14:
+                        Console.WriteLine("Exiting Program...");
                         break;
                     default:
                         Console.WriteLine("Invalid choice. Please try again.");
                         break;
-                    Console.Read();
+                Console.Read();
                 }
             }
         }
     }
 }
-    
-
-
